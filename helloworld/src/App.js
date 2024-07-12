84035242dc17de8ef63fdb823732af916910ca8e
John Clyde <johnclyde@gmail.com>
2024-07-12 16:29:06 -0700

    Component Directory (#4)

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ComponentDirectory from './ComponentDirectory';
+import InteractiveCounter from './InteractiveCounter';
 
 const App = () => {
-  return <h1>Hello, World!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ComponentDirectory />} />
+        <Route path="/interactive-counter" element={<InteractiveCounter />} />
+        {/* Add more routes here as you create new components */}
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
