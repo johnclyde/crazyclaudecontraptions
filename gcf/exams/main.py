8dfa922245b6fccff4dd61d18be75185d813b3d8
John Clyde <john.clyde@mozartdata.com>
2024-07-13 02:17:33 -0700

    clean up

--- gcf/exams/main.py
+++ gcf/exams/main.py
@@ -3,6 +3,7 @@ from google.cloud import firestore_v1
 
 db = firestore_v1.Client(database="grindolympiads")
 
+
 def list_exams(request: Request) -> Response:
     headers = {"Access-Control-Allow-Origin": "*"}
 
@@ -18,16 +19,18 @@ def list_exams(request: Request) -> Response:
                 for exam in exams:
                     exam_name = exam.id
                     exam_doc = exam.document("Info").get()
-                    if not exam_doc.exists or not exam_doc.to_dict().get("private", False):
-                        tests.append({
-                            "competition": competition_name,
-                            "year": year_name,
-                            "exam": exam_name
-                        })
+                    if not exam_doc.exists or not exam_doc.to_dict().get(
+                        "private", False
+                    ):
+                        tests.append(
+                            {
+                                "competition": competition_name,
+                                "year": year_name,
+                                "exam": exam_name,
+                            }
+                        )
 
-        response_data = {
-            "tests": tests
-        }
+        response_data = {"tests": tests}
         return jsonify(response_data), 200, headers
 
     except Exception as e:
