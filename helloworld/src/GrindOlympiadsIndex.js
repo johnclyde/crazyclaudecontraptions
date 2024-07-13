4f661db4fa9a5f35a6353fc8e1120b3df8a388ee
John Clyde <john.clyde@mozartdata.com>
2024-07-13 00:54:19 -0700

    variables

--- helloworld/src/GrindOlympiadsIndex.js
+++ helloworld/src/GrindOlympiadsIndex.js
@@ -47,6 +47,14 @@ const GrindOlympiadsIndex = () => {
 
     fetchData();
 
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+
+    if (error) {
+      return <div>Error: {error}</div>;
+    }
+
     // Fetch notifications
     fetch('/api/notifications')
       .then(response => response.json())
