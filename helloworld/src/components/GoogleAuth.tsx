f10659c51e0b62758b5c7dea8b568d736c735559
John Clyde <john.clyde@mozartdata.com>
2024-07-14 20:49:26 -0700

    blech

--- helloworld/src/components/GoogleAuth.tsx
+++ helloworld/src/components/GoogleAuth.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React from "react";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 
 interface GoogleAuthProps {
