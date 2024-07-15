10589525b41bcc8f3c2c5bd08803acf72a3ed719
John Clyde <john.clyde@mozartdata.com>
2024-07-14 20:33:03 -0700

    noooo

--- helloworld/src/components/GoogleAuth.tsx
+++ helloworld/src/components/GoogleAuth.tsx
@@ -1,10 +1,5 @@
 import React, { useEffect, useState, useCallback } from "react";
-
-declare global {
-  interface Window {
-    google: any;
-  }
-}
+import { GoogleOAuthProvider, GoogleLogin } from "react-oauth/google";
 
 interface GoogleAuthProps {
   onSuccess: (response: any) => void;
@@ -12,50 +7,11 @@ interface GoogleAuthProps {
 }
 
 const GoogleAuth: React.FC<GoogleAuthProps> = ({ onSuccess, onFailure }) => {
-  const [isScriptLoaded, setIsScriptLoaded] = useState(false);
-
-  const handleCredentialResponse = useCallback(
-    (response: any) => {
-      if (response.credential) {
-        onSuccess(response);
-      } else {
-        onFailure("Google Sign-In failed");
-      }
-    },
-    [onSuccess, onFailure],
-  );
-
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://accounts.google.com/gsi/client";
-    script.async = true;
-    script.defer = true;
-    script.onload = () => setIsScriptLoaded(true);
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
-  useEffect(() => {
-    console.log("Client ID:", process.env.REACT_APP_GOOGLE_CLIENT_ID);
-    if (isScriptLoaded && window.google) {
-      window.google.accounts.id.initialize({
-        client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-        callback: handleCredentialResponse,
-      });
-      window.google.accounts.id.renderButton(
-        document.getElementById("googleSignInButton"),
-        { theme: "outline", size: "large" },
-      );
-    }
-  }, [isScriptLoaded, handleCredentialResponse]);
-
+  console.log("Client ID:", process.env.REACT_APP_GOOGLE_CLIENT_ID);
   return (
-    <div>
-      <div id="googleSignInButton"></div>
-    </div>
+    <GoogleOAuthProvider clientId={clientId!}>
+      <GoogleLogin onSuccess={handleSuccess} onError={handleFailure} />
+    </GoogleOAuthProvider>
   );
 };
 
