d24a3bdf45dfdf19b6f70e301ac43e99043379ef
John Clyde <john.clyde@mozartdata.com>
2024-07-15 16:21:06 -0700

    ok

--- helloworld/src/components/GrindOlympiadsLayout.js
+++ helloworld/src/components/GrindOlympiadsLayout.js
@@ -1,25 +1,26 @@
 import React from "react";
-import Header from "./Header";
-import useUserData from "../hooks/useUserData";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
 import useNotifications from "../hooks/useNotifications";
+import useUserData from "../hooks/useUserData";
 
-const GrindOlympiadsLayout = ({ children }) => {
-  const { user, isLoggedIn, login, logout } = useUserData();
-  const { notifications, notificationsError, markNotificationAsRead } =
-    useNotifications();
+const GrindOlympiadsLayout: React.FC = () => {
+  const { user, isLoggedIn, setIsLoggedIn, login, logout } = useUserData();
+  const { notifications, notificationsError, markNotificationAsRead } = useNotifications();
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-gray-100">
       <Header
         user={user}
         isLoggedIn={isLoggedIn}
         notifications={notifications}
+        setIsLoggedIn={setIsLoggedIn}
         notificationsError={notificationsError}
         markNotificationAsRead={markNotificationAsRead}
         login={login}
         logout={logout}
       />
-      <main className="flex-grow">{children}</main>
+      <main className="flex-grow"><Outlet /></main>
     </div>
   );
 };
