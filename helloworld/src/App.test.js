2314f0887c27070d6d0fdcdc18c70433b145e868
John Clyde <john.clyde@mozartdata.com>
2024-07-16 13:56:57 -0700

    test this stuff

--- helloworld/src/App.test.js
+++ helloworld/src/App.test.js
@@ -1,8 +1,31 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import App from "./App";
+import { mockAuth } from "./setupTests";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Mock the UserDataContext
+jest.mock("./contexts/UserDataContext", () => ({
+  UserDataProvider: ({ children }) => children,
+  useUserData: () => ({
+    user: null,
+    isLoggedIn: false,
+    login: jest.fn(),
+    logout: jest.fn(),
+  }),
+}));
+
+test("renders App without crashing", () => {
+  mockAuth.onAuthStateChanged.mockImplementation((callback) => callback(null));
+
+  render(
+    <BrowserRouter>
+      <GoogleOAuthProvider clientId="dummy-client-id">
+        <App />
+      </GoogleOAuthProvider>
+    </BrowserRouter>,
+  );
 });
---
+++ helloworld/src/mocks/handlers.js
@@ -0,0 +1,7 @@
+import { rest } from 'msw';
+
+export const handlers = [
+  rest.post('https://us-central1-olympiads.cloudfunctions.net/logout', (req, res, ctx) => {
+    return res(ctx.json({ message: 'Logout successful' }));
+  }),
+];
