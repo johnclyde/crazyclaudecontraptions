c58fdfd618cbe7bdff62efe74249d73086452547
John Clyde <john.clyde@mozartdata.com>
2024-07-16 14:01:38 -0700

    This is a lot.

--- helloworld/src/mocks/handlers.js
+++ helloworld/src/mocks/handlers.js
@@ -1,7 +1,10 @@
-import { rest } from 'msw';
+import { rest } from "msw";
 
 export const handlers = [
-  rest.post('https://us-central1-olympiads.cloudfunctions.net/logout', (req, res, ctx) => {
-    return res(ctx.json({ message: 'Logout successful' }));
-  }),
+  rest.post(
+    "https://us-central1-olympiads.cloudfunctions.net/logout",
+    (req, res, ctx) => {
+      return res(ctx.json({ message: "Logout successful" }));
+    },
+  ),
 ];
--- helloworld/src/setupTests.js
+++ helloworld/src/setupTests.js
@@ -1,9 +1,10 @@
 import "@testing-library/jest-dom";
 import { TextEncoder, TextDecoder } from "util";
-import { server } from './mocks/server';
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+
+import { server } from "./mocks/server";
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
