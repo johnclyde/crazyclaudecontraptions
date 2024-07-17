79847f4be97776e8cdc4328600ef927ae5ec0db0
John Clyde <johnclyde@gmail.com>
2024-07-17 09:41:56 +0000

    Don't load notifications when logged out.

--- helloworld/src/firebase.js
+++ helloworld/src/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "olympiads-ba812.firebaseapp.com",
+  authDomain: "congenial-space-enigma-jrv7wq6rgf596j.github.dev",
   projectId: "olympiads",
   storageBucket: "olympiads.appspot.com",
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
