d4fb5d7107b1bd312f21e9b812e23179f4ed7b35
John Clyde <johnclyde@gmail.com>
2024-07-17 03:55:28 -0700

    Run ReactJS tests. (#41)

--- helloworld/src/setupTests.js
+++ helloworld/src/setupTests.js
@@ -1,5 +1,22 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+import { TextEncoder, TextDecoder } from "util";
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+jest.mock("firebase/auth", () => ({
+  getAuth: jest.fn(),
+  signInWithPopup: jest.fn(),
+  signOut: jest.fn(),
+  GoogleAuthProvider: jest.fn(),
+}));
+
+jest.mock("firebase/app", () => {
+  const auth = {
+    onAuthStateChanged: jest.fn(),
+  };
+  return {
+    initializeApp: jest.fn(),
+    auth: jest.fn(() => auth),
+  };
+});
