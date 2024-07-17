98199b61bed251ae34e8e6cc49514308a6e6ecb7
John Clyde <johnclyde@gmail.com>
2024-07-17 10:01:17 +0000

    mock stuff

--- helloworld/src/setupTests.js
+++ helloworld/src/setupTests.js
@@ -3,3 +3,10 @@ import { TextEncoder, TextDecoder } from "util";
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+
+jest.mock("firebase/auth", () => ({
+  getAuth: jest.fn(),
+  signInWithPopup: jest.fn(),
+  signOut: jest.fn(),
+  GoogleAuthProvider: jest.fn(),
+}));
