9f8e30bc0012e98dcedcc0e48d5f0c7a99be18c2
John Clyde <johnclyde@gmail.com>
2024-07-17 09:58:03 +0000

    ehhhh

--- helloworld/src/setupTests.js
+++ helloworld/src/setupTests.js
@@ -1,6 +1,5 @@
 import "@testing-library/jest-dom";
 import { TextEncoder, TextDecoder } from "util";
-import { server } from './mocks/server';
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
