5bceecbe3eea921b39ff2dbfcf4744cbdd7b5652
John Clyde <john.clyde@mozartdata.com>
2024-07-24 20:44:00 -0700

    Add ProtectedRoute.tsx

---
+++ helloworld/src/components/ProtectedRoute.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { useUserDataContext } from "../contexts/UserDataContext";
+
+interface ProtectedRouteProps {
+  adminOnly?: boolean;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ adminOnly = false }) => {
+  const { isLoggedIn, user } = useUserDataContext();
+
+  if (!isLoggedIn) {
+    return <Navigate to="/" replace />;
+  }
+
+  if (adminOnly && !user?.isAdmin) {
+    return <Navigate to="/" replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default ProtectedRoute;
