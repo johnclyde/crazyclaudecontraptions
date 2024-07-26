86f73203dee69cbab98ffa7975c85aaffe99d8b5
John Clyde <johnclyde@gmail.com>
2024-07-26 19:53:58 +0000

    blah

--- helloworld/src/components/LatexRenderer.tsx
+++ helloworld/src/components/LatexRenderer.tsx
@@ -34,34 +34,49 @@ const LatexRenderer: React.FC<LatexRendererProps> = ({
   const renderContent = () => {
     const parts = latex.split(/(__OPTION_[A-E]__)/);
 
-    return parts.map((part, index) => {
+    // Process the parts to create a map of options
+    const options = {};
+    let latexPart = "";
+
+    parts.forEach((part, index) => {
       if (part.startsWith("__OPTION_") && part.endsWith("__")) {
-        // This is an option identifier
-        const option = part.replace(/__OPTION_([A-E])__/, "$1");
-        const isSelected = selectedOption === option;
-        return (
-          <button
-            key={index}
-            onClick={() => onOptionClick && onOptionClick(option)}
-            className={`px-2 py-1 mr-2 border rounded focus:outline-none focus:ring ${
-              isSelected
-                ? "bg-green-200 border-green-500"
-                : "hover:bg-gray-100 focus:border-blue-300"
-            }`}
-          >
-            ({option})
-          </button>
-        );
-      } else {
-        return <MathJax key={index} inline={false}>{`${part}`}</MathJax>;
+        options[part] = parts[index + 1].trim();
+      } else if (
+        !parts[index - 1] ||
+        !parts[index - 1].startsWith("__OPTION_")
+      ) {
+        latexPart += part;
       }
     });
+
+    return (
+      <div>
+        <div style={{ display: "inline-block" }}>
+          {Object.keys(options).map((optionKey) => (
+            <span
+              key={`option-${optionKey}`}
+              style={{ display: "inline-block", margin: "0 8px" }}
+            >
+              <button
+                onClick={() => onOptionClick(optionKey)}
+                className={`px-2 py-1 mr-2 mb-2 border rounded focus:outline-none focus:ring ${
+                  selectedOption === optionKey
+                    ? "bg-green-200 border-green-500"
+                    : "hover:bg-gray-100 focus:border-blue-300"
+                }`}
+              >
+                {optionKey}
+              </button>
+              {options[optionKey]}
+            </span>
+          ))}
+        </div>
+      </div>
+    );
   };
 
   return (
-    <MathJaxContext config={mathJaxConfig}>
-      <div>{renderContent()}</div>
-    </MathJaxContext>
+    <MathJaxContext config={mathJaxConfig}>{renderContent()}</MathJaxContext>
   );
 };
 
