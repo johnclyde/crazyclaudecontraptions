33fa7885fe3245b63ca06598062c62a14413140e
John Clyde <john.clyde@mozartdata.com>
2024-07-26 18:34:29 -0700

    start

--- helloworld/src/App.test.tsx
+++ helloworld/src/App.test.tsx
@@ -1,6 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+const originalConsoleError = console.error;
+beforeAll(() => {
+  console.error = jest.fn();
+});
+afterAll(() => {
+  console.error = originalConsoleError;
+}
+
+jest.mock("../firebase", () => ({
+  auth: {
+    onAuthStateChanged: jest.fn(() => jest.fn()),
+  },
+}));
+
 test("renders GrindOlympiads header", () => {
   render(<App />);
   const headerElement = screen.getByText(/GrindOlympiads/i);
