a22e3cd79ca44116b4f51a88899d4e8b256d7bb7
John Clyde <john.clyde@mozartdata.com>
2024-07-26 19:10:07 -0700

    try these?

--- helloworld/src/App.test.tsx
+++ helloworld/src/App.test.tsx
@@ -1,12 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-jest.mock("react-router-dom", () => ({
-  ...jest.requireActual("react-router-dom"),
-  useNavigate: () => jest.fn(),
-  BrowserRouter: ({ children }) => <div>{children}</div>,
-}));
-
 jest.mock("../firebase", () => ({
   auth: {
     onAuthStateChanged: jest.fn((callback) => {
@@ -16,8 +10,17 @@ jest.mock("../firebase", () => ({
   },
 }));
 
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => jest.fn(),
+}));
+
 test("renders GrindOlympiads header", () => {
-  render(<App />);
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
   const headerElement = screen.getByText(/GrindOlympiads/i);
   expect(headerElement).toBeInTheDocument();
 });
