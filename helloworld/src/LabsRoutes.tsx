4e300d955f247b8817710980a1b1400df723c37e
John Clyde <johnclyde@gmail.com>
2024-08-01 19:29:21 -0700

    Add Challenges Component. (#107)

--- helloworld/src/LabsRoutes.tsx
+++ helloworld/src/LabsRoutes.tsx
@@ -6,6 +6,7 @@ import ColorPicker from "./components/ColorPicker";
 import InteractiveCounter from "./components/InteractiveCounter";
 import ProblemEditorDemo from "./components/ProblemEditorDemo";
 import LoginWarningPopup from "./components/LoginWarningPopup";
+import Challenges from "./components/Challenges";
 
 const LabsRoutes: React.FC = () => {
   return (
@@ -16,6 +17,7 @@ const LabsRoutes: React.FC = () => {
       <Route path="color-picker" element={<ColorPicker />} />
       <Route path="problem-editor-demo" element={<ProblemEditorDemo />} />
       <Route path="login-warning-popup" element={<LoginWarningPopup />} />
+      <Route path="challenges" element={<Challenges />} />
     </Routes>
   );
 };
